d3fbc11da209b8c7e3a70a82aa661859abd211b1
zzangouona <zzangouona@estsecurity.com>
2023-01-10 19:20:00 +0900

    feat : color 지정

--- src/theme/colors/type.ts
+++ src/theme/colors/type.ts
@@ -1,8 +1,21 @@
 import { KeysOfUnion } from '@src/utils';
-import { allColors } from './constants';
+import { allColors, status } from './constants';
 
 type ThemeType = 'light' | 'dark';
 type allColorsType = typeof allColors;
 type allColorsKeys = keyof typeof allColors;
+
+enum colorKeyEnum {
+  Common = 'common',
+  status = 'status',
+  // color
+  Gray = 'gray',
+  Orange = 'orange',
+  Teal = 'teal',
+}
+
+type grayRange = 50 | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
+type orangeRage = 50 | 100 | 200 | 300 | 400 | 500 | 600 | 700;
+type tealRage = 50 | 100 | 200 | 300 | 400 | 500 | 600 | 700;
 
 export type { ThemeType, allColorsType, allColorsKeys };
