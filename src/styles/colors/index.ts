94ee7fdac75612d5f650f7dc435a4a6c332b76a2
zzangouona <yajung0203@naver.com>
2023-01-12 04:13:12 +0900

    중간커밋

--- src/styles/colors/index.ts
+++ src/styles/colors/index.ts
@@ -1,27 +1,30 @@
 import { color } from './colors';
-import { KeysOfUnion, cssVar } from '@src/utils';
+import { KeysOfUnion, cssVar } from '@/utils';
 import { light, dark } from './palettes';
-import { Theme } from './type';
+import { Theme, ColorObj, ColorGroup } from './type';
 
 export const ThemeColorSets: Theme = {
-  dark: { ...dark },
   light: { ...light },
+  dark: { ...dark },
 };
 
-const buildCssVariables = <T extends Object, K>(variables: T) => {
-  const colorListArray = Object.entries(variables)
-    .map((color) => Object.keys(color[1]).map((value) => `${color[0]}-${value}`))
-    .flat() as [keyof K];
-  return colorListArray;
+const buildCssVariables = <T extends Object>(variables: T) => {
+  const colorListArray = Object.entries(variables).map((color) => {
+    const keys = Object.keys(color[1]);
+    return keys.reduce(
+      (acc, key) => acc.concat(`--${color[0]}-${key}: ${color[1][key]};`, '\n'),
+      '',
+    );
+  });
+  return colorListArray.join();
 };
-
 export const colorThemes = {
   light: buildCssVariables(ThemeColorSets.light),
   dark: buildCssVariables(ThemeColorSets.dark),
   common: buildCssVariables(color),
 };
 
-const buildColorPalette = <T extends Object>(variables: T) => {
+const buildColorPalette2 = <T extends Object>(variables: T) => {
   const keys = Object.keys(variables) as KeysOfUnion<T>[];
   return keys.reduce((acc, current) => {
     acc[current] = cssVar(current);
@@ -29,4 +32,17 @@ const buildColorPalette = <T extends Object>(variables: T) => {
   }, {} as Record<KeysOfUnion<T>, string>);
 };
 
+const buildColorPalette = <T extends Object>(variables: T) => {
+  const colorListArray = Object.entries(variables).map((color) => {
+    const keys = Object.keys(color[1]);
+    return keys.reduce(
+      (acc, key) =>   acc[current]
+    return acc;
+  }, {} as Record<KeysOfUnion<T>, string>);
+};
+
 //export const cssVar = (name: string) => `var(--${name.replace(/_/g, '-')})`;
+
+export const colorListArray = Object.entries(color)
+  // .map(color => Object.keys(color[1]).map(value => `${color[0]}-${value}`))
+  .flat() as ColorList[];
