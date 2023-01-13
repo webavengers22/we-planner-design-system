9a1fcd3c6583ac81af8c656a6e023eac80204154
zzangouona <zzangouona@estsecurity.com>
2023-01-13 18:02:29 +0900

    중간커밋

--- src/styles/colors/constant.ts
+++ src/styles/colors/constant.ts
@@ -1,17 +1,36 @@
 import { baseColor } from './colors';
-import { dark } from './dark';
-import { light } from './light';
-import { ThemeMode as ThemeColorType } from '@/types/themeType';
-import { PalletteDefine, ColorObject } from './type';
+import { darkColor } from './dark';
+import { lightColor } from './light';
+import { BaseColorList } from './type';
 
-const paletteColor: PalletteDefine = {
-  common: { ...baseColor },
-  light: { ...light },
-  dark: { ...dark },
+const paletteColor = {
+  standard: { ...baseColor },
+  light: { ...lightColor },
+  dark: { ...darkColor },
 };
 const themeColor = {
   light: { ...paletteColor.light },
   dark: { ...paletteColor.light },
 };
+
+export const BaseColorListArray = Object.entries(baseColor)
+  .map((color) => Object.keys(color[1]).map((value) => `${color[0]}-${value}`))
+  .flat() as BaseColorList[];
+
+const test = Object.entries(colors).reduce<Record<string, ColorProperty>>(
+  (acc, [title, object]) => {
+    Object.keys(object).map((data) => {
+      const test = {
+        title: `${title}-${data}`,
+        name: `--${title}-${data}\n`,
+        value: object[data],
+        css: `var(--${title}-${data})`,
+      };
+      acc[`${title}-${data}`] = test;
+    });
+    return acc;
+  },
+  {},
+);
 
 export { paletteColor, themeColor };
