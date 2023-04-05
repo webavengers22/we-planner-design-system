f54ec08a3df7856aa31b08539367e4922d798756
zzangouona <zzangouona@estsecurity.com>
2023-04-05 14:41:15 +0900

    feat : vite 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import VitePluginHtmlEnv from 'vite-plugin-html-env';
+import tsconfigpaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigpaths(), VitePluginHtmlEnv(), svgr()],
   resolve: {
     alias: [
       { find: '@', replacement: resolve(__dirname, 'src') },
